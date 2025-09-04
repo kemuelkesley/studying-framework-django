e1e4126f771beb80e8dec1c66b0d997197c6df50
kemuelkesley <kemuelkesleytkd@outlook.com>
2025-09-04 11:10:34 -0300

    add-page recipe-view.html

--- recipes/views.py
+++ recipes/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={'name': 'Kemuel'})
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={'name': 'kemuel kesley'})
