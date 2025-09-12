c8c1cf58af7341bbfe1bd05ca10cd6d5536fe567
kemuelkesley <kemuelkesleytkd@outlook.com>
2025-09-12 15:55:20 -0300

    add-inserindo dados dentro do template.

--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
+from utls.recipes.factory import make_recipe
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'recipes/pages/home.html', context={'name': 'Kemuel'})
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': [make_recipe() for _ in range(10)]
+    })
 
 
 def recipe(request, id):
-    return render(request, 'recipes/pages/recipe-view.html', context={'name': 'kemuel kesley'})
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': make_recipe(),
+    })
